ea85db5e4c419cd30af9ea10518be1e2669089dd
besttottnoi <besttottnoi@hotmail.com>
2025-01-20 12:51:06 +0700

    Add files via upload

---
+++ SipSample_swift/SipSample/ContactCell.swift
@@ -0,0 +1,19 @@
+//
+//  ContactCell.m
+//  SIPSample
+//
+//  Created by Joe Lepple on 6/14/13.
+//  Copyright (c) 2013 PortSIP Solutions, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+    @IBOutlet var urlLabel: UILabel!
+    @IBOutlet var noteLabel: UILabel!
+    @IBOutlet var onlineImageView: UIImageView!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ SipSample_swift/SipSample/SipSample-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <PortSIPVoIPSDK/PortSIPVoIPSDK.h>
+#import "Reachability.h"
